ed1dd098da78797020b7dfe18649c8ae5c05e3ca
rokas <rookas.rudzenskas@gmail.com>
2024-11-14 13:12:09 +0100

    Fix

--- app/(tabs)/boxes.tsx
+++ app/(tabs)/boxes.tsx
@@ -5,7 +5,19 @@ import {Text, View, StyleSheet} from 'react-native';
 const Boxes = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <View style={{width: 100, height: 100, backgroundColor: 'red', boxShadow: '10px 10px 5px green'}}/>
+      <View style={{
+        width: 100,
+        height: 100,
+        backgroundColor: 'red',
+        boxShadow: '10px 10px 10px gray, -10px -10px 10px lime'
+        // boxShadow: {
+        //   offsetX: 5,
+        //   offsetY: 5,
+        //   blurRadius: 10,
+        //   spreadDistance: 5,
+        //   color: 'green',
+        // },
+      }}/>
     </View>
   );
 };
