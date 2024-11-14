2e245c28b495f968e6a38d3107cbf6aa431c2c1c
rokas <rookas.rudzenskas@gmail.com>
2024-11-14 14:30:38 +0100

    fix

---
+++ app/(tabs)/dom.tsx
@@ -0,0 +1,12 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import WebComponent from "@/components/WebComponent";
+
+const Dom = () => {
+  return (
+    <WebComponent />
+  );
+};
+
+export default Dom;
--- components/WebComponent.tsx
+++ components/WebComponent.tsx
@@ -1,6 +1,9 @@
+"use dom";
+
 //@ts-nocheck
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import RichTextEditor from 'rich-text-editor'
 
 const WebComponent = () => {
   return (
@@ -8,6 +11,11 @@ const WebComponent = () => {
       <h1>I am Rokas</h1>
       <p>Hello, I'm a React Native Web component!</p>
       <button>Click me!</button>
+      <RichTextEditor
+        // value={value}
+        // onChange={setValue}
+        placeholder="Type something..."
+      />
     </div>
   );
 };
